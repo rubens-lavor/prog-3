6d688c7e30b4f7f8599e03e9ff05329788b8c798
rubens-lavor <haryson07@gmail.com>
2020-09-06 23:37:00 -0300

    separação dos inscritos por curso

--- atividade 1/vestibular/main.cpp
+++ atividade 1/vestibular/main.cpp
@@ -39,18 +39,18 @@ int busca_numero(char *vetor_de_busca, int pos_contra_barra, int qtde_pos, int p
 
         if (unidade == 10 || dezena == 10 || centena == 10) //tratamento para nota > 1000
         {
-            cout << " Opa! estamos no if que trata nota<1000 " << endl;
+            //cout << " Opa! estamos no if que trata nota<1000 " << endl;
             if (centena == 10)
             {
                 unidade = milhar;
-                cout << " nota com 1 dígito " << endl;
+                //cout << " nota com 1 dígito " << endl;
                 return unidade - 48;
             }
             else if (dezena == 10)
             {
                 unidade = centena;
                 dezena = milhar;
-                cout << " nota com 2 dígitos " << endl;
+                //cout << " nota com 2 dígitos " << endl;
                 return (dezena - 48) * 10 + (unidade - 48);
             }
             else
@@ -59,7 +59,7 @@ int busca_numero(char *vetor_de_busca, int pos_contra_barra, int qtde_pos, int p
                 unidade = dezena;
                 dezena = centena;
                 centena = milhar;
-                cout << " nota com 3 dígitos " << endl;
+                //cout << " nota com 3 dígitos " << endl;
                 return (centena - 48) * 100 + (dezena - 48) * 10 + (unidade - 48);
             }
         }
@@ -82,7 +82,7 @@ void criaLista()
     //arquivo << "nº inscrição|\tIdade|\tCurso Pretendido|\tPontuação";
     //arquivo << "\n";
 
-    for (int i = 1; i <= 3; i++)
+    for (int i = 1; i <= 10; i++)
     {
         int registro = 1000 + i;
         int idade = numero_aleatorio(30, 17);
@@ -107,65 +107,64 @@ void criaLista()
     arquivo.close();
 }
 
-void ordena(int *vet, int *vet_guarda_posicao)
+void ordena(int *vet)
 {
 
-    int maior[2];
+    int maior[4];
     //int auxmaior;
-    int posicao[2];
+    int posicao[4];
     //int auxpos;
     bool troca;
-    int aux[2];
+    int aux[4];
     //int aux2 = 0;
 
-
-    for (int i = 0; i < 6; i+=2)
+    for (int i = 0; i < 40; i += 4)
     {
         troca = false;
         maior[0] = vet[i];
         posicao[0] = i;
-        
 
-        
-
-        for (int j = i + 2; j < 6; j+=2)
+        for (int j = i + 4; j < 40; j += 4)
         {
 
             if (vet[j] > maior[0])
             {
 
                 maior[0] = vet[j];
-                maior[1] = vet[j+1];
-                posicao[0] = j;
-                posicao[1] = j+1;
-                troca = true;
+                maior[1] = vet[j + 1];
+                maior[2] = vet[j + 2];
+                maior[3] = vet[j + 3];
 
+                posicao[0] = j;
+                posicao[1] = j + 1;
+                posicao[2] = j + 2;
+                posicao[3] = j + 3;
+
+                troca = true;
             }
-            
-            
-            
-            
         }
 
         if (troca)
         {
 
             aux[0] = vet[i];
-            aux[1] = vet[i+1];
+            aux[1] = vet[i + 1];
+            aux[2] = vet[i + 2];
+            aux[3] = vet[i + 3];
 
             vet[i] = maior[0];
-            vet[i+1] = maior[1];
+            vet[i + 1] = maior[1];
+            vet[i + 2] = maior[2];
+            vet[i + 3] = maior[3];
 
             vet[posicao[0]] = aux[0];
             vet[posicao[1]] = aux[1];
-
+            vet[posicao[2]] = aux[2];
+            vet[posicao[3]] = aux[3];
         }
-        
-        
-        
-    
     }
 
+    /*
     cout << endl;
     cout << endl;
 
@@ -174,6 +173,17 @@ void ordena(int *vet, int *vet_guarda_posicao)
 
         cout << vet[i] << endl;
     }
+    */
+}
+
+void curso_escolhido(int *vetor_curso, int registro, int nota, int idade, int curso, int contador)
+{
+    int indice = contador * 4;
+
+    vetor_curso[indice] = nota;
+    vetor_curso[indice + 1] = registro;
+    vetor_curso[indice + 2] = idade;
+    vetor_curso[indice + 3] = curso;
 }
 int main()
 {
@@ -183,19 +193,17 @@ int main()
     int caractere_por_linha = 0;
     int contra_barra = 0;
 
-    char vetor_de_linha_e_pontuacao[2] = {};
+    //char vetor_de_linha_e_pontuacao[2] = {};
 
-    int vetor_nota_registro_idade_curso[12] = {};
+    int vetor_nota_registro_idade_curso[40] = {};
 
-    int vetor_pos_contra_barra[10] = {};
+    int vetor_pos_contra_barra[15] = {}; //alterar quando aumentar
 
-    int vetor_manobra[6] = {};
+    int vetor_manobra[40] = {};
 
-    int pos_contra_barra = 0;
+    //int pos_contra_barra = 0;
 
-    int vet_guarda_posicao[4] = {};
-
-    
+    //int vet_guarda_posicao[4] = {};
 
     criaLista();
 
@@ -243,11 +251,6 @@ int main()
     cout << "contra barra " << contra_barra << endl;
     cout << vetor_de_busca << endl;
 
-    for (int i = 0; i < 4; i++)
-    {
-        cout << vetor_de_busca[i + 68];
-        linha[i] = vetor_de_busca[i + 68];
-    }
     cout << endl;
 
     int nota = 0;
@@ -255,13 +258,26 @@ int main()
     int idade = 0;
     int curso = 0;
     int indice = 0;
+    int contador = 0;
+    int vetor_curso_11[40] = {};
+    int vetor_curso_12[40] = {};
+    int vetor_curso_13[40] = {};
+    int vetor_curso_14[40] = {};
+    int vetor_curso_15[40] = {};
+    int vetor_curso_16[40] = {};
 
-    for (int i = 0; i < 3; i++)
+    for (int i = 0; i < 10; i++)
     {
         nota = busca_numero(vetor_de_busca, vetor_pos_contra_barra[i], 4, 15);
         registro = busca_numero(vetor_de_busca, vetor_pos_contra_barra[i], 4, 2);
         idade = busca_numero(vetor_de_busca, vetor_pos_contra_barra[i], 2, 7);
         curso = busca_numero(vetor_de_busca, vetor_pos_contra_barra[i], 2, 11);
+
+        if (curso == 11)
+        {
+            curso_escolhido(vetor_curso_11, registro, nota, idade, curso, contador);
+            contador++;
+        }
 
         indice = i * 4;
         //cout <<"resultado de indice"<< indice << endl;
@@ -271,80 +287,58 @@ int main()
         vetor_nota_registro_idade_curso[indice + 2] = idade;
         vetor_nota_registro_idade_curso[indice + 3] = curso;
 
-        vetor_manobra[i*2] = nota;
-        vetor_manobra[i*2+1] = registro;
+        vetor_manobra[indice] = nota;
+        vetor_manobra[indice + 1] = registro;
+        vetor_manobra[indice + 2] = idade;
+        vetor_manobra[indice + 3] = curso;
 
         cout << "nota: " << vetor_nota_registro_idade_curso[indice];
         cout << "\tregistro: " << vetor_nota_registro_idade_curso[indice + 1];
         cout << "\tidade: " << vetor_nota_registro_idade_curso[indice + 2];
         cout << "\tcurso " << vetor_nota_registro_idade_curso[indice + 3] << endl;
-
-
-    }
-
-    cout << endl;
-
-    for (int i = 0; i < 12; i++)
-    {
-
-        cout << vetor_nota_registro_idade_curso[i] << " - ";
     }
 
     cout << endl;
     cout << endl;
-
-    for (int i = 0; i < 6; i++)
-    {
-
-        cout << vetor_manobra[i] << endl;
-    }
 
     //ordenar
-    ordena(vetor_manobra, vet_guarda_posicao);
+    ordena(vetor_manobra);
 
-    /*
-    int nota = busca_numero(vetor_de_busca, vetor_pos_contra_barra[0], 4, 15);
-    int nota2 = busca_numero(vetor_de_busca, vetor_pos_contra_barra[1], 4, 15);
-    int nota3 = busca_numero(vetor_de_busca, vetor_pos_contra_barra[2], 4, 15);
+    cout << "inscrição"
+         << "\t"
+         << "nota"
+         << "\t"
+         << "idade"
+         << "\t"
+         << "curso" << endl;
 
-    int registro = busca_numero(vetor_de_busca, vetor_pos_contra_barra[0], 4, 2);
-    int registro2 = busca_numero(vetor_de_busca, vetor_pos_contra_barra[1], 4, 2);
-    int registro3 = busca_numero(vetor_de_busca, vetor_pos_contra_barra[2], 4, 2);
-    
-    int idade = busca_numero(vetor_de_busca, vetor_pos_contra_barra[0], 2, 7);
-    int idade2 = busca_numero(vetor_de_busca, vetor_pos_contra_barra[1], 2, 7);
-    int idade3 = busca_numero(vetor_de_busca, vetor_pos_contra_barra[2], 2, 7);
-
-    int curso = busca_numero(vetor_de_busca, vetor_pos_contra_barra[0], 2, 11);
-    int curso2 = busca_numero(vetor_de_busca, vetor_pos_contra_barra[1], 2, 11);
-    int curso3 = busca_numero(vetor_de_busca, vetor_pos_contra_barra[2], 2, 11);
-    
-    */
+    for (int i = 0; i < 10; i++)
+    {
+        cout << vetor_manobra[i * 4 + 1] << "\t\t" << vetor_manobra[i * 4] << "\t" << vetor_manobra[i * 4 + 2] << "\t" << vetor_manobra[i * 4 + 3] << endl;
+    }
 
     cout << endl;
+    cout << endl;
+
+    cout << "\t\tinscrição"
+         << "\t"
+         << "nota"
+         << "\t"
+         << "idade"
+         << "\t"
+         << "curso" << endl;
+
+    for (int i = 0; i < 5; i++)
+    {
+        cout << "teste curso 11 " << vetor_curso_11[i * 4 + 1] << "\t\t" << vetor_curso_11[i * 4] << "\t" << vetor_curso_11[i * 4 + 2] << "\t" << vetor_curso_11[i * 4 + 3] << endl;
+    }
 
     /*
-
-    cout << " nota 1 " << nota;
-    cout << "\t idade 1 " << idade;
-    cout << "\t curso " << curso;
-    cout << "\t registro 1 " << registro << endl;
-    
-    
-    cout << " nota 2 " << nota2;
-    cout << "\t idade 2 " << idade2;
-    cout << "\t curso " << curso2;
-    cout << "\t registro 2 " << registro2 << endl;
-
-    cout << " nota 3 " << nota3;
-    cout << "\t idade 3 " << idade3;
-    cout << "\t curso " << curso3;
-    cout << "\t registro 3 " << registro3 << endl;
-    */
 
     cout << " vetor_pos_contra_barra[0] " << vetor_pos_contra_barra[0] << endl;
     cout << " vetor_pos_contra_barra[1] " << vetor_pos_contra_barra[1] << endl;
     cout << " vetor_pos_contra_barra[2] " << vetor_pos_contra_barra[2] << endl;
+    */
 
     return 0;
 }
