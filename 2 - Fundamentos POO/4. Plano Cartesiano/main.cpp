f8caba5f0f289f1c7d0fa6bc373ee2ab8822ac7c
rubens-lavor <haryson07@gmail.com>
2020-09-28 14:41:24 -0300

    refatoração

--- 2 - Fundamentos POO/4. Plano Cartesiano/main.cpp
+++ 2 - Fundamentos POO/4. Plano Cartesiano/main.cpp
@@ -1,12 +1,7 @@
 #include <iostream>
 #include <cmath>
-#include <vector>
-#include <utility>
-#include <array>
-#include <algorithm>
 
-class Pontos
-{
+class Pontos{
 private:
     float x{0};
     float y{0};
@@ -39,8 +34,7 @@ public:
     }
 };
 
-class Triangulo
-{
+class Triangulo{
 private:
     Pontos *pontos;
 
@@ -49,50 +43,23 @@ public:
     ~Triangulo(){}
 
     int perimetro(Pontos p1, Pontos p2, Pontos p3){
-        float lado1;
-        float lado2;
-        float lado3;
 
-        //float perimetro;
-        float componente_x = 0;
-        float componente_y = 0;
+        float perimetro = 0;
 
-        componente_x = p2.get_x() - p1.get_x();
-        componente_y = p2.get_y() - p1.get_y();
-        lado1 = sqrt(pow(componente_x,2)+pow(componente_y,2));
-
-        //std::cout << "Lado 1: " << lado1 << std::endl;
-
-        componente_x = p3.get_x() - p2.get_x();
-        componente_y = p3.get_y() - p2.get_y();
-        lado2 = sqrt(pow(componente_x,2)+pow(componente_y,2));
-
-        //std::cout << "Lado 2: " << lado2 << std::endl;
+        perimetro+= distancia_entre_dois_pontos(p1,p2);
+        perimetro+= distancia_entre_dois_pontos(p2,p3);
+        perimetro+= distancia_entre_dois_pontos(p3,p1);
 
 
-        componente_x = p3.get_x() - p1.get_x();
-        componente_y = p3.get_y() - p1.get_y();
-        lado3 = sqrt(pow(componente_x,2)+pow(componente_y,2));
+        return perimetro;
+    }
 
-        //std::cout << "Lado 3: " << lado3 << std::endl;
-
-
-
-        /*
-        lado1 = sqrt(pow(p1.get_x(),2)+pow(p1.get_y(),2));
-        lado2 = sqrt(pow(p2.get_x(),2)+pow(p2.get_y(),2));
-        lado3 = sqrt(pow(p3.get_x(),2)+pow(p3.get_y(),2));
-        */
-
-        return (lado1 + lado2 + lado3);
+    float distancia_entre_dois_pontos(Pontos p1, Pontos p2){
+        return sqrt(pow(p2.get_x() - p1.get_x(),2) + pow(p2.get_y() - p1.get_y(),2));
     }
 };
 
 int main(){
-
-    //std::cout << pow(2,0) << std::endl;
-
-    //exit(1);
 
     Pontos p1(0.0,0.0);
     Pontos p2(sqrt(3.0),sqrt(6.0));
@@ -100,7 +67,7 @@ int main(){
 
     Triangulo t;
 
-    std::cout << " O perímetro é: " << t.perimetro(p1,p2,p3) << std::endl;
+    std::cout << "O perímetro é: " << t.perimetro(p1,p2,p3) << std::endl;
 
    return 0;
     
