38d5c7e7daf0e6a1d34fd953b04a422f82ac5823
rubens-lavor <haryson07@gmail.com>
2020-09-29 23:17:50 -0300

    dateandtime

---
+++ 2 - Fundamentos POO/7. DateAndTime/_Date.hpp
@@ -0,0 +1,138 @@
+#include <iostream>
+#include <vector>
+
+class Date
+{
+private:
+    int _dia{1};
+    int mes{1};
+    int ano{2000};
+
+    std::string data{};
+
+    int dias_mes{31};
+
+public:
+    Date(int dia, int mes, int ano)
+    {
+        setDia(dia);
+        setMes(mes);
+        setAno(ano);
+    }
+    Date()
+    {
+    }
+
+    ~Date()
+    {
+    }
+
+    void nextDay()
+    {
+
+        if (this->_dia + 1 > dias_mes)
+        {
+            setDia(1);
+
+            if (this->mes + 1 > 12)
+            {
+                setMes(1);
+                setAno(getAno() + 1);
+            }
+            else
+            {
+                setMes(getMes() + 1);
+            }
+        }
+        else
+        {
+            setDia(getDia() + 1);
+        }
+    }
+
+    void setAno(int ano)
+    {
+        if (ano < 0)
+            return;
+        this->ano = ano;
+    }
+
+    int getAno()
+    {
+
+        return this->ano;
+    }
+
+    void setMes(int mes)
+    {
+
+        if (mes < 13 && mes > 0)
+        {
+
+            if (mes == 4 || mes == 6 || mes == 9 || mes == 11)
+            {
+                this->dias_mes = 30;
+            }
+            else if (mes == 2)
+            {
+                this->dias_mes = 28;
+            }
+            else
+            {
+                this->dias_mes = 31;
+            }
+
+            this->mes = mes;
+        }
+        else
+        {
+            std::cout << "mês inválido \n";
+        }
+    }
+
+    int
+    getMes()
+    {
+
+        return this->mes;
+    }
+
+    void setDia(int dia)
+    {
+        if (dia > this->dias_mes || dia < 0)
+        {
+            std::cout << "dia inválido \n";
+            return;
+        }
+
+        this->_dia = dia;
+    }
+
+    int getDia()
+    {
+
+        return this->_dia;
+    }
+
+    std::string completa_data(int x)
+    {
+        return (
+            std::to_string(x).length() == 1 ? "0" + std::to_string(x) + "/" : std::to_string(x) + "/");
+    }
+
+    void imprime_data()
+    {
+
+        data = {};
+
+        data.append(completa_data(getDia()));
+        data.append(completa_data(getMes()));
+        data.append(completa_data(getAno()));
+
+        data.pop_back();
+
+        std::cout << data << std::endl;
+        //std::cout << "dias_mes" << dias_mes << std::endl;
+    }
+};
+
---
+++ 2 - Fundamentos POO/7. DateAndTime/main.cpp
@@ -0,0 +1,69 @@
+#include "_Date.hpp"
+#include "_Time.hpp"
+
+class DateAndTime
+{
+private:
+    Date date;
+    Time time;
+
+public:
+    DateAndTime(Date d, Time t){
+        this->date = d;
+        this->time = t;
+        time.horario_universal();
+        date.imprime_data();
+    }
+
+    DateAndTime(){}
+
+    ~DateAndTime(){}
+
+    void mostra_data_e_hora(){   
+        time.horario_universal();
+        date.imprime_data();
+    }
+
+    void tick(){
+        if (time.getSegundo() + 1 == 60){
+            time.setSegundo(0);
+
+            if (time.getMinuto() + 1 == 60){
+                time.setMinuto(0);
+
+                if (time.getHora() + 1 == 24){
+                    date.nextDay();
+                    time.setHora(0);
+                }
+                else{
+                    time.setHora(time.getHora() + 1);
+                }
+            }
+            else{
+                time.setMinuto(time.getMinuto() + 1);
+            }
+        }
+        else{
+            time.setSegundo(time.getSegundo() + 1);
+        }
+    }
+};
+
+int main(){
+    Date d(31, 1, 2020);
+    Time t(23, 59, 59);
+
+    DateAndTime dt(d,t);
+
+    std::cout<<"\ntick()..\n";
+    dt.tick();
+
+    dt.mostra_data_e_hora();
+
+    std::cout<<"\ntick()..\n";
+    dt.tick();
+
+    dt.mostra_data_e_hora();
+
+    return 0;
+}
