045bdc2bdd876abb343fd220b813f5d262a8bea6
rubens-lavor <haryson07@gmail.com>
2020-10-01 23:42:49 -0300

    update

--- 2 - Fundamentos POO/9. Exercício 9/main.cpp
+++ 2 - Fundamentos POO/9. Exercício 9/main.cpp
@@ -1,8 +1,150 @@
 #include <iostream>
 #include <vector>
-#include "Funcionario.hpp"
+//#include "Funcionario.hpp"
 ///#include "Departamento.hpp"
-#include "Empresa.hpp"
+//#include "Empresa.hpp"
+
+class Funcionario
+{
+private:
+    std::string nome;
+    float salario{0};
+    std::string data_admissao{"dd/mm/aa"};
+
+public:
+    Funcionario(/* args */);
+    Funcionario(std::string nome)
+    {
+        this->nome = nome;
+    }
+    ~Funcionario()
+    {
+    }
+    void setSalario(float valor)
+    {
+        this->salario = valor;
+    }
+    float getSalario()
+    {
+        return this->salario;
+    }
+    void setNome(std::string nome)
+    {
+        this->nome = nome;
+    }
+
+    std::string getNome()
+    {
+        return this->nome;
+    }
+    /*
+    void setNome(std::string);
+    std::string getNome();
+    void setSalario(float valor);
+    float getSalario();
+    */
+
+    void setAdmissao(std::string data)
+    {
+        this->data_admissao = data;
+    }
+    std::string getAdmissa()
+    {
+        return this->data_admissao;
+    }
+};
+
+class Departamento
+{
+private:
+    std::string nome;
+    Funcionario _funcionario;
+    std::vector<Funcionario> vetor_funcionarios;
+
+public:
+
+    Departamento(std::string nome)
+    {
+        this->nome = nome;
+    }
+
+    Departamento(/* args */)
+    {
+    }
+
+    ~Departamento()
+    {
+    }
+    void aumento()
+    {
+
+        for (auto v:vetor_funcionarios)
+        {
+            v.setSalario(v.getSalario() * 1.1);
+            std::cout << v.getNome() << " - " << v.getSalario() << std::endl;
+        }
+    }
+    void setFuncionario(Funcionario fun, float salario, std::string data_adm)
+    {
+        this->_funcionario = fun;
+
+        this->_funcionario.setSalario(salario);
+        this->_funcionario.setAdmissao(data_adm);
+        vetor_funcionarios.push_back(this->_funcionario);
+    }
+
+    void mostra_funcionarios()
+    {
+        std::cout << "Funcionários do " << this->nome << ":" << std::endl;
+
+        for (auto v : vetor_funcionarios)
+        {
+            std::cout << v.getNome() << " - " << v.getSalario() << std::endl;
+        }
+
+        std::cout << std::endl;
+    }
+
+    void setNome(std::string nome)
+    {
+        this->nome = nome;
+    }
+    std::string getNome()
+    {
+        return this->nome;
+    }
+};
+
+
+class Empresa
+{
+private:
+    std::string nome;
+    std::string CNPJ;
+    std::vector<Departamento> departamento;
+
+public:
+    Empresa(/* args */){}
+    ~Empresa(){}
+
+    void setDepartamento(Departamento d)
+    {
+        if (departamento.size() == 10)
+        {
+            std::cout << "Número máximo de departamentos\n";
+            return;
+        }
+        departamento.push_back(d);
+    }
+
+    void mostra_depar(){
+        for (auto v : departamento)
+    {
+        std::cout << v.getNome() << " - " << std::endl;
+    }
+    }
+
+};
 
 int main()
 {
