ce3c7e9853deca31dc5d2b6464e80cfe7731c3c6
rubens-lavor <haryson07@gmail.com>
2020-10-08 20:25:03 -0300

    finalizado

--- 3 - Construtores e Destrutores/Time/main.cpp
+++ 3 - Construtores e Destrutores/Time/main.cpp
@@ -3,154 +3,60 @@
 
 class Time {
    private:
-    std::vector<std::pair<int, int>> horarios{{},{},{}}; //HH,MM,SS
+    //std::vector<std::pair<int, int>> horarios{{}, {}, {}};  //HH,MM,SS
+    
+    
     std::string horario{};
+    int hora {};
+    int minuto {};
+    int segundo {};
+
 
    public:
 
-   /*
-    Money() : amount{ 0.0 } {};
-    Money(double _amount) : amount{ _amount } {};
-    */
-
-    explicit Time():horario{"00:00:00"}, horarios{{0, 0}, {0, 0}, {0, 0}} {};
-
-    Time(int hora, int min){
-        setHora(hora);
-        setMinuto(min);
-        setSegundo(0);
+    //construtor explicito
+    explicit Time() : horario{"00:00:00"} {
+        std::cout << "Construtor explícito padrão" << std::endl;
     }
 
+    //Construtor que inicializa como parâmetros as horas e os minutos
+    Time(int hora, int min) {
+        std::cout << "Construtor que inicia com hora e minuto" << std::endl;
+    }
+
+    //Destrutor
     ~Time() {
         std::cout << "Destrutor\n";
     }
 
     explicit Time(int segundo) {
-        setHora(0);
-        setMinuto(0);
-        setSegundo(segundo);
-
-        std::cout<<"contrutor de conversão int para Time"<< std::endl;
+        std::cout << "Construtor de conversão int para Time" << std::endl;
     }
 
-    Time (const Time &t){
-        std::cout<<"contrutor de cópia"<< std::endl;
+    Time(const Time &t) {
+        std::cout << "Construtor de cópia" << std::endl;
     }
 
     explicit Time(double hora) {
-        setHora(hora);
-        setMinuto(0);
-        setSegundo(0);
-
-        std::cout<<"contrutor de conversão double para Time"<< std::endl;
-    }
-
-    void tick() {
-        if (getSegundo() + 1 == 60) {
-            setSegundo(0);
-
-            if (getMinuto() + 1 == 60) {
-                setMinuto(0);
-
-                if (getHora() + 1 == 24) {
-                    setHora(0);
-                } else {
-                    setHora(getHora() + 1);
-                }
-            } else {
-                setMinuto(getMinuto() + 1);
-            }
-        } else {
-            setSegundo(getSegundo() + 1);
-        }
-    }
-
-    void setHora(int hora) {
-        if (hora >= 24 || hora < 0) {
-            return;
-        }
-
-        horarios[0].first = hora / 10;
-        horarios[0].second = hora % 10;
-    }
-
-    int getHora() {
-        return (horarios[0].first * 10 + horarios[0].second);
-    }
-
-    void setMinuto(int minuto) {
-        if (minuto > 60 || minuto < 0) {
-            return;
-        }
-
-        horarios[1].first = minuto / 10;
-        horarios[1].second = minuto % 10;
-    }
-
-    int getMinuto() {
-        return (horarios[1].first * 10 + horarios[1].second);
-    }
-
-    void setSegundo(int segundo) {
-        if (segundo > 60 || segundo < 0) {
-            return;
-        }
-
-        horarios[2].first = segundo / 10;
-        horarios[2].second = segundo % 10;
-    }
-
-    int getSegundo() {
-        return (horarios[2].first * 10 + horarios[2].second);
-    }
-
-    void imprime_hora(std::string s = "") {
-        horario.append(s);
-        std::cout << horario << std::endl;
-    }
-
-    void for_horarios() {
-        for (auto t : horarios) {
-            horario.append(std::to_string(t.first) + std::to_string(t.second) + ":");
-        }
-
-        horario.pop_back();
-    }
-
-    void horario_universal() {
-        horario = {};
-
-        for_horarios();
-
-        imprime_hora();
-    }
-
-    std::string completa_horario(int x)  //essa função edita o formato da hora com 2 digitos caso o zero esteja a esquerda
-    {
-        return (
-            std::to_string(x).length() == 1 ? "0" + std::to_string(x) + ":" : std::to_string(x) + ":");
-    }
-
-    void horario_AM_PM() {
-        horario = {};
-
-        if (getHora() > 12) {
-            horario.append(completa_horario(getHora() - 12));
-            horario.append(completa_horario(getMinuto()));
-            horario.append(completa_horario(getSegundo()));
-
-            horario.pop_back();
-        }
-
-        else {
-            for_horarios();  //essa função preenche a string horarios.
-        }
-
-        imprime_hora(getHora() > 12 ? " PM" : " AM");
+        std::cout << "Construtor de conversão double para Time" << std::endl;
     }
 };
 
+void construtor_de_conversao(const Time t) {
+}
+
+void construtor_copia(const Time t){
+
+}
+
 int main() {
+
+    Time t{};
+    t = {2, 3};
+
+    construtor_copia(t);
+    construtor_de_conversao((Time)1);
+    construtor_de_conversao((Time)1.0);
 
     return 0;
 }
