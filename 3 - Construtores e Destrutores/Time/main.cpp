2ca279ab7807a8c63c0c9ebc1b5e848cc105a5cc
rubens-lavor <haryson07@gmail.com>
2020-10-08 20:33:04 -0300

    update

--- 3 - Construtores e Destrutores/Time/main.cpp
+++ 3 - Construtores e Destrutores/Time/main.cpp
@@ -1,19 +1,14 @@
 #include <iostream>
-#include <vector>
 
 class Time {
    private:
-    //std::vector<std::pair<int, int>> horarios{{}, {}, {}};  //HH,MM,SS
-    
-    
+    //std::vector<std::pair<int, int>> horarios{{}, {}, {}};
     std::string horario{};
-    int hora {};
-    int minuto {};
-    int segundo {};
-
+    int hora{};
+    int minuto{};
+    int segundo{};
 
    public:
-
     //construtor explicito
     explicit Time() : horario{"00:00:00"} {
         std::cout << "Construtor explícito padrão" << std::endl;
@@ -29,15 +24,20 @@ class Time {
         std::cout << "Destrutor\n";
     }
 
+    //Construtor explícito de conversão de int para Time
     explicit Time(int segundo) {
+        this->segundo=segundo;
         std::cout << "Construtor de conversão int para Time" << std::endl;
     }
 
+    //Construtor de cópia
     Time(const Time &t) {
         std::cout << "Construtor de cópia" << std::endl;
     }
 
+    //Construtor explícito de conversão de double para Time
     explicit Time(double hora) {
+        this->hora=hora;
         std::cout << "Construtor de conversão double para Time" << std::endl;
     }
 };
@@ -45,47 +45,18 @@ class Time {
 void construtor_de_conversao(const Time t) {
 }
 
-void construtor_copia(const Time t){
-
+void construtor_copia(const Time t) {
 }
 
 int main() {
-
     Time t{};
-    t = {2, 3};
+    Time t2 = {2, 3};
+    std::cout << "\n";
 
     construtor_copia(t);
     construtor_de_conversao((Time)1);
     construtor_de_conversao((Time)1.0);
 
+    std::cout << "\n";
     return 0;
 }
-
-/*
-#include <iostream>
-
-class Money
-{
-public:
-    Money() : amount{ 0.0 } {};
-    Money(double _amount) : amount{ _amount } {};
-
-    double amount;
-};
-
-void display_balance(const Money balance)
-{
-    std::cout << "The balance is: " << balance.amount << std::endl;
-}
-
-int main(int argc, char* argv[])
-{
-    Money payable{ 79.99 };
-
-    display_balance(payable);
-    display_balance(49.95);
-    display_balance(9.99f);
-
-    return 0;
-}
-*/
