2d392d4fc12d9f4063a93fe4c07edb17cde944dd
rubens-lavor <haryson07@gmail.com>
2020-10-25 01:16:56 -0300

    refatoração

--- 5 - Sobrecarga de Operadores/feetinches1.cpp
+++ 5 - Sobrecarga de Operadores/feetinches1.cpp
@@ -1,24 +1,21 @@
 #include "feetinches1.h"
 
 void FeetInches::simplify() {
-    int cont = 0;
     if (this->inches > 0) {
         while (this->inches > 12) {
-            cont++;
+            this->feet ++;
             this->inches -= 12;
         }
-        this->feet += cont;
     } else {
         while (this->inches < 0) {
-            cont++;
+            this->feet --;
             this->inches += 12;
         }
-        this->feet -= cont;
     }
 }
 
 FeetInches FeetInches::operator+(const FeetInches &a) {
-    return FeetInches(feet + a.getFeet(),inches + a.getInches());
+    return FeetInches(feet + a.getFeet(), inches + a.getInches());
 }
 FeetInches FeetInches::operator-(const FeetInches &a) {
     return FeetInches(feet - a.getFeet(), inches - a.getInches());
