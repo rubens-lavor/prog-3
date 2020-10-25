b3ff5f5c71c555ea5eedd8e7f91e279967560717
rubens-lavor <haryson07@gmail.com>
2020-10-25 02:15:35 -0300

    exercício 3

---
+++ 5 - Sobrecarga de Operadores/exercício 3/over3.cc
@@ -0,0 +1,34 @@
+ #include <iostream>
+ #include "feetinches3.h"
+ using namespace std;
+ int main()
+ {
+ int feet, inches;
+ // To hold input for feet and inches
+ // Create two FeetInches objects. The default arguments
+ // for the constructor will be used.
+ FeetInches first, second;
+ // Get a distance from the user.
+ cout << "Enter a distance in feet and inches: ";
+ cin >> feet >> inches;
+ // Store the distance in first.
+ first.setFeet(feet);
+ first.setInches(inches);
+ // Get another distance.
+ cout << "Enter another distance in feet and inches: ";
+ cin >> feet >> inches;
+ // Store the distance in second.
+ second.setFeet(feet);
+ second.setInches(inches);
+ // Compare the two objects.
+ if (first == second)
+ cout << "first is equal to second.\n";
+ if (first > second)
+ cout << "first is greater than second.\n";
+ if (first < second)
+ cout << "first is less than second.\n";
+ return 0;
+ 
+     
+}
+ 
