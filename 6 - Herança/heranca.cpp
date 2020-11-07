c0b3e557b9bc90b3820a96e958dc7a985afa4a73
rubens-lavor <haryson07@gmail.com>
2020-11-06 21:14:32 -0300

    update

--- 6 - Herança/heranca.cpp
+++ 6 - Herança/heranca.cpp
@@ -7,59 +7,52 @@ metodos da classe BASE e suas funcoes amigas
 seja acessı́vel por membros e funções amigas da classe base e
 derivada
 */
-#include <string>
 #include <iostream>
+#include <string>
 using namespace std;
- 
-class Person
-{
-public:
-   string m_name;
-   int m_age;
+
+class Person {
+   public:
+    string m_name;
+    int m_age;
     Person(string name = "", int age = 0)
-    //construtor em lista
-        : m_name(name), m_age(age)
-    {
+        //construtor em lista
+        : m_name(name), m_age(age) {
     }
     string getName() const { return m_name; }
     int getAge() const { return m_age; }
 };
 
 /*Classe BaseballPlayer herdou pblicamente de Person*/
-class BaseballPlayer : public Person
-{
-public:
+class BaseballPlayer : public Person {
+   public:
     double m_battingAverage;
     int m_homeRuns;
- 
+
     BaseballPlayer(double battingAverage = 0.0, int homeRuns = 0)
-    //lista de inicializacao do construtor tem que ter : no inicio
-       : m_battingAverage(battingAverage), m_homeRuns(homeRuns)
-    {
+        //lista de inicializacao do construtor tem que ter : no inicio
+        : m_battingAverage(battingAverage), m_homeRuns(homeRuns) {
     }
-    void imprime(){
-    // Cout do nome
- // Utiliza getName da classe BASE pois esse metodo
- //esta publico para a classe derivada
- cout << getName() << '\n'; 
-  // Utiliza m_battingAverage e m_homeRuns da classe derivada
-  // pois eh um objeto da classe derivada
- cout <<"Rebatidas: " << m_battingAverage << '\n'; //
- cout << "Home Runs: "<<m_homeRuns<< '\n'; 
+    void imprime() {
+        // Cout do nome
+        // Utiliza getName da classe BASE pois esse metodo
+        //esta publico para a classe derivada
+        cout << getName() << '\n';
+        // Utiliza m_battingAverage e m_homeRuns da classe derivada
+        // pois eh um objeto da classe derivada
+        cout << "Rebatidas: " << m_battingAverage << '\n';  //
+        cout << "Home Runs: " << m_homeRuns << '\n';
     }
 };
 
-
-int main()
-{
- // Cria um jogador joe
- BaseballPlayer joe;
- // Nomeia o jogador utilizando atributo m_name da classe BASE. Note que esse parametro
- //esta publico para a classe derivada 
- joe.m_name = "Joe";
- joe.imprime();
- return 0;
- 
+int main() {
+    // Cria um jogador joe
+    BaseballPlayer joe;
+    // Nomeia o jogador utilizando atributo m_name da classe BASE. Note que esse parametro
+    //esta publico para a classe derivada
+    joe.m_name = "Joe";
+    joe.imprime();
+    return 0;
 }
 
 /* Exercicio 1: modifique as classes para que os parametros sejam privados,
