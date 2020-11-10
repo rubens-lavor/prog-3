53ee894caa9661d9c0d59174bdf33272d70200dd
rubens-lavor <haryson07@gmail.com>
2020-11-09 22:03:38 -0300

    apresentação

--- seminário/main.jl
+++ seminário/main.jl
@@ -1,41 +1,8 @@
-#declaração de classe em julia
-type nome_da_classe
-    atributo1::int8
-    atributo2 #não é obrigatório especificar o tipo
-
-    metodo1::Function
-    metodo2 #não é obrigatório especificar que se trata de uma função
-  
-	#as especificações são feitas dentro do construtor
-	
-    #construtor
-    function nome_da_classe(at1,at2)
-
-        atributo1 = at1
-        atributo2 = at2
-
-        self=new() 
-        #instância da própria classe, 
-        #que obrigatoriamente é feita dentro do construtor
-
-        self.metodo1 = function(parametros) #metodo1 recebe uma função
-            #código
-        end
-
-        self.metodo2 = function() #metodo2 recebe uma função
-            #código
-        end #fim da função
-
-        return self 
-        # é necessário retornar o objeto criado para que 
-        # sejam atribuídos aos métodos suas funções.
-        
-    end #fim do construtor
-
-end #fim da classe
+compilador online: https://repl.it/languages/julia
 
 
-
+#Exemplo Classe, métodos, objeto
+ 
 mutable struct Exemplo
     atributo1::Int8
     atributo2
@@ -67,3 +34,78 @@ end
 
 obj1 = Exemplo(43,"Olá Mundo!")
 funcao_exemplo(obj1)
+
+
+
+#--------------------------------------------
+
+x = (sqrt ∘ +)(3, 6)
+
+exemplo = map(first ∘ reverse ∘ uppercase, split("you can compose functions like this"))
+
+exemplo = 1:10 |> sum |> sqrt
+
+exemplo2 = (sqrt ∘ sum)(1:10)
+
+struct Data
+    dia::Int
+     mes::Int
+     ano::Int
+end
+
+d = Data (14,2,1996)
+vetor = [10 ,20 ,30 ,40 ,50]
+
+i = 2
+vetor2 =[2* i ,5 ,3* i ]
+
+
+#-------------------------------------------
+
+
+#exemplo de sobrecarga de operadores#
+function Base.:+()
+  println("Sobrecarga do Operador +")
+end
+
+let x = 1
+   if x == 1
+      +()
+   end
+end
+
+#-------------------------#
+#Exemplo operadores sao funcoes#
+println(1+2+3)
+
+println(+(1,2,3))
+
+f = +
+println(f(1,2,3))
+
+#-------------------------#
+#exemplo polimorfismo por sobrecarga#
+function soma( f1 :: Float64 , f2 :: Float64 )
+  f1 + f2
+end
+function soma( i1 :: Int64 , i2 :: Int64 )
+  i1 + i2
+end
+println(soma(2.1,3.1))
+println(soma(1,2))
+
+#-------------------------#
+#exemplo polimorfismo parametrico#
+function parametrico( x :: Any )
+  x
+end
+struct Point
+  x :: Int
+  y :: Int
+end
+p = Point(1,2)
+
+println(parametrico(p))
+println(parametrico(21))
+println(parametrico(13.7))
+
